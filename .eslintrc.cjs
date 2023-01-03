12ff79c1b9d792ed12557265f1cfe25bbd2b924a
Lucas Demea <lucas@digital-swing.com>
2023-01-03 21:44:53 +0100

    build: bump build tools

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,45 +1,44 @@
-module.exports = {
-  root: true,
-  plugins: ['import', 'jest'],
-  extends: [
-    'eslint:recommended',
-    'plugin:jest/recommended',
-    'plugin:jest/style',
-    'plugin:prettier/recommended',
-  ],
+const tsConfig = require('./tsconfig.json');
+const tsTestConfig = require('./tsconfig.test.json');
 
-  parser: '@babel/eslint-parser',
+module.exports = {
   env: {
-    node: true,
-    es6: true,
     amd: true,
     browser: true,
+    es6: true,
     jquery: true,
-    jest: true,
-    'jest/globals': true,
+    node: true,
   },
-  parserOptions: {
-    ecmaFeatures: {
-      globalReturn: true,
-      generators: false,
-      objectLiteralDuplicateProperties: false,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
+  extends: ['eslint:recommended', 'prettier'],
+  ignorePatterns: [
+    '**/node_modules/*.[tj]s',
+    '**/vendor/*.[tj]s',
+    '**/dist/**/*.[tj]s',
+    '**/public/**/*.[tj]s',
+    '**/build/**/*.[tj]s',
+  ].concat(tsConfig.exclude),
   overrides: [
     {
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
+      ],
+      files: tsConfig.include,
       parser: '@typescript-eslint/parser',
-      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      // Your TypeScript files extension
       parserOptions: {
-        project: ['./tsconfig.test.json'], // Specify it only for TypeScript files
-        ecmaFeatures: {
-          globalReturn: true,
-          generators: false,
-          objectLiteralDuplicateProperties: false,
-        },
-        ecmaVersion: 2020,
-        sourceType: 'module',
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+
+      plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-tsdoc'],
+    },
+
+    {
+      env: {
+        jest: true,
+        'jest/globals': true,
       },
       extends: [
         'eslint:recommended',
@@ -47,56 +46,118 @@ module.exports = {
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:prettier/recommended',
       ],
+      files: tsTestConfig.include,
+
+      parser: '@typescript-eslint/parser',
+      // Your TypeScript files extension
+      parserOptions: {
+        project: ['./tsconfig.test.json'], // Specify it only for TypeScript files
+      },
+      plugins: ['jest'],
+    },
+    {
+      files: ['src/plugins/**'],
+      rules: {
+        'no-param-reassign': [
+          'error',
+          { ignorePropertyModificationsFor: ['ctx'], props: true },
+        ],
+      },
     },
   ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    babelOptions: {
+      configFile: './.babelrc.cjs',
+    },
+    ecmaFeatures: {
+      generators: false,
+      globalReturn: true,
+      objectLiteralDuplicateProperties: false,
+    },
+    ecmaVersion: 'latest',
+    extraFileExtensions: ['.cjs'],
+    sourceType: 'module',
+  },
+  plugins: ['import', 'prettier', 'sort-keys-fix', 'typescript-sort-keys'],
+  root: true,
+  rules: {
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore',
+        imports: 'always-multiline',
+        objects: 'always-multiline',
+      },
+    ],
+
+    // 'no-undef': 'warn',
+    'func-names': 0,
+
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          'webpack.config.cjs',
+          'webpack.prod.cjs',
+          'webpack.dev.cjs',
+          'rollup.config.ts',
+          'setupTestFrameworkScriptFile.ts',
+          '.eslintrc.cjs',
+          '__tests__/**/*',
+          '__mocks__/**/*',
+        ],
+      },
+    ],
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    'no-param-reassign': [
+      'error',
+      {
+        ignorePropertyModificationsFor: ['state'],
+        props: true,
+      },
+    ],
+    'no-underscore-dangle': 0,
+    'sort-imports': [
+      'warn',
+      {
+        allowSeparatedGroups: false,
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+    'sort-keys-fix/sort-keys-fix': 'warn',
+
+    'tsdoc/syntax': 'warn',
+    'typescript-sort-keys/interface': 'warn',
+    'typescript-sort-keys/string-enum': 'warn',
+  },
   settings: {
     'import/core-modules': [],
     'import/ignore': [
       'node_modules',
       '\\.(coffee|scss|css|less|hbs|svg|json)$',
     ],
-  },
-  ignorePatterns: [
-    'ripple.js',
-    '**/node_modules/*.[tj]s',
-    '**/vendor/*.[tj]s',
-    '**/dist/**/*.[tj]s',
-    '**/public/**/*.[tj]s',
-    '**/build/**/*.[tj]s',
-  ],
-  rules: {
-    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-console': 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-underscore-dangle': 0,
-    // 'no-undef': 'warn',
-    'func-names': 0,
-    'comma-dangle': [
+    'import/order': [
       'error',
       {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'ignore',
-      },
-    ],
-    'no-param-reassign': [
-      'error',
-      {
-        props: true,
-        ignorePropertyModificationsFor: ['state'],
-      },
-    ],
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: [
-          '**/webpack.mix.cjs',
-          '**/webpack.utils.js',
-          '__tests__/**/*',
-          '__mocks__/**/*',
-          'config.ts',
+        groups: [
+          'index',
+          'sibling',
+          'parent',
+          'internal',
+          'external',
+          'builtin',
+          'object',
+          'type',
         ],
       },
     ],
